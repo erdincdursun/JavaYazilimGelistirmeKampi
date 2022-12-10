8aa9314d4465e6163ba228819dfed1af0b7d6714
Erdinç Dursun <113687007+erdincdursun@users.noreply.github.com>
2022-12-10 06:54:36 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,44 @@
-import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+import bussines.CategoryManager;
+import bussines.CourseManager;
+import bussines.TeacherManager;
+import core.loggin.DataBaseLogger;
+import core.loggin.FileLogger;
+import core.loggin.Logger;
+import dataaAcces.HibernateCategoryDao;
+import dataaAcces.HibernateCourseDao;
+import dataaAcces.HibernateTeacherDao;
+import dataaAcces.JdbcCourseDao;
+import entities.Category;
+import entities.Course;
+import entities.Teacher;
 
 public class Main {
-    public static void main(String[] args) {
-        ekle();
-        sil();
-        güncelle();
-        topla(12,15);
-        int toplam =   topla(12,15,15,20,30,40,50);
-        System.out.println(toplam);
 
-    }
+	public static void main(String[] args) throws Exception {
+		
+		Logger[] loggers = {new FileLogger()};
+		
+		Course course1 = new Course(4,"Kotlin","BusraDursun","YazilimKampi1",3000); 
+		CourseManager courseManager = new CourseManager(new JdbcCourseDao(),loggers);
+		courseManager.add(course1);
+		
+		System.out.println("-------------------");
+		
+		Category Category1 = new Category(5,"Bilisim");
+		CategoryManager categoryManager = new CategoryManager(new HibernateCategoryDao(), loggers);
+		categoryManager.add(Category1);
+		
+		System.out.println("-------------------");
+		
+		Teacher teacher = new Teacher("Busra","Dursun");
+		TeacherManager teacherManager = new TeacherManager(new HibernateTeacherDao(), loggers);
+		teacherManager.add(teacher);
+		
 
-    public static void ekle() {
-        System.out.println("Eklendi");
-    }
-
-    public static void sil() {
-        System.out.println("silindi");
-    }
-
-    public static void güncelle() {
-        System.out.println("Güncellendi");
-    }
-    public static int topla(int... sayilar){
-        int toplam=0;
-        for(int sayi : sayilar){
-            toplam += sayi;
-
-        }
-        return toplam;
-    }
+		
+	}
 
 }
-
